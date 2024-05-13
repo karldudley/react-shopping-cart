cab7f87bba4e6424836608a5242ee39b1b73f97b
Karl Dudley <karldudley@gmail.com>
2024-05-14 00:15:27 +0100

    first commit

---
+++ src/dummy-products.js
@@ -0,0 +1,59 @@
+import watch1 from './assets/watch1.jpg';
+import watch2 from './assets/watch2.jpg';
+import watch3 from './assets/watch3.jpg';
+import watch4 from './assets/watch4.jpg';
+import watch5 from './assets/watch5.jpg';
+import watch6 from './assets/watch6.jpg';
+
+export const DUMMY_PRODUCTS = [
+  {
+    id: 'p1',
+    image: watch1,
+    title: 'White Rider Analog Watch at 2:12 Time',
+    price: 129.99,
+    description:
+      'Channel timeless sophistication with this stunning mocha watch. Crafted for the discerning gentleman who appreciates the fine blend of vintage charm and modern elegance.',
+  },
+  {
+    id: 'p2',
+    image: watch2,
+    title: 'Black Apple Watch With Black Sports Band',
+    price: 189.99,
+    description:
+      'Bask in the glow of elegance with the Black Apple Watch. Embody the regality of a king with a sweet touch of cyan that whispers enchantment. This is the perfect piece for those seeking to create unforgettable moments.',
+  },
+
+  {
+    id: 'p3',
+    image: watch3,
+    title: 'Round Silver-colored Analog Watch With Black Strap',
+    price: 49.99,
+    description:
+      'Brace the showers in style! Our vintage splash-proof watch ensures that you stand out, even in the dullest weather. Because rain is never a reason to compromise on your fashion quotient.',
+  },
+  {
+    id: 'p4',
+    image: watch4,
+    title:
+      'Shallow Focus Photography of Silver-colored Casio Chronograph Watch',
+    price: 249.99,
+    description:
+      'Step into the boardroom with unmatched confidence with our classic Casio Chronograph Watch. Exuding an air of refined class and understated power, it is ideal for the modern executive who values tradition and elegance.',
+  },
+  {
+    id: 'p5',
+    image: watch5,
+    title: 'Chronograph Watch',
+    price: 159.99,
+    description:
+      'Sweep the room off its feet in our Chronograph Watch. Crafted to mimic the allure of the moonlight, this watch is a nod to those who appreciate subtle glamour and a standout silhouette.',
+  },
+  {
+    id: 'p6',
+    image: watch6,
+    title: 'Black Leather Strap Silver Chronograph Watch',
+    price: 79.99,
+    description:
+      'Our Black Leather Strap Silver Chronograph Watch is a tribute to those who embody the spirit of adventure. Designed with durability and comfort in mind, this watch is a wardrobe essential for the urban explorer.',
+  },
+];
